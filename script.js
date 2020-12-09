7d54423ed4844c63c7c4c8af65dc960138f54668
manuel <manuelmolinuevo22@gmail.com>
2020-12-09 10:46:15 -0300

    Barra Progreso

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const reproducir = document.getElementById("reproductor");
 const btnBack = document.querySelector(".fa-backward");
 const btnFor = document.querySelector(".fa-forward");
 const barraProg = document.getElementById("barra");
+const tiempoCancion = document.getElementById("tiempo");
+const duracionCancion = document.getElementById("duracion");
 
 var canciones = new Array();
 canciones[0]= "/canciones/atrapando-sueños/La Beriso - Cantemos.mp3";
@@ -29,6 +31,7 @@ function togglePlayPause(){
 
 function reproducirCancion(){
     reproducir.src = `${canciones[numeroCancion]}`;
+    avanzarCancion();
     if(songPlaying === true){
         reproducir.pause();
         reproducir.currentTime = 0;
@@ -36,14 +39,18 @@ function reproducirCancion(){
     reproducir.play();
     btnPlay.removeEventListener("click", reproducirCancion);
     }
-    avanzarCancion();
 }
 
 function avanzarCancion(){
     reproducir.addEventListener("ended", ()=>{
-        numeroCancion += 1;
+        if(numeroCancion === 5){
+            numeroCancion = 0;
+        } else {
+            numeroCancion += 1;
+        }
         reproducir.src = `${canciones[numeroCancion]}`;
         reproducir.play();
+        
     });
 }
 
@@ -95,11 +102,35 @@ function backBtn(){
 
 }
 
+function cambiarPosicion(){
+    reproducir.currentTime = (+barraProg.value * reproducir.duration) / 100;
+}
 
+function actualizarTiempo(){
+    barraProg.value = (reproducir.currentTime / reproducir.duration) * 100;
+
+    let mins = Math.floor(reproducir.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    let secs = Math.floor(reproducir.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+    let duracionMin = Math.floor(reproducir.duration / 60);
+    let duracionSeg = Math.floor(reproducir.duration % 60);
+    if (duracionSeg < 10) {
+        duracionSeg = '0' + String(duracionSeg);
+    }
+
+    tiempoCancion.innerHTML = `${mins}:${secs}`;
+    duracionCancion.innerHTML = `0${duracionMin}:${duracionSeg}`;
+}
 
 
 btnPlay.addEventListener("click", reproducirCancion);
 btnBack.addEventListener("click",backBtn);
 btnFor.addEventListener("click", siguienteCancion);
-
-
+barraProg.addEventListener("change", cambiarPosicion);
+reproducir.addEventListener("timeupdate", actualizarTiempo);
